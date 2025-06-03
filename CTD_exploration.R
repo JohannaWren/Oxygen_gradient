798c66d11619d96db7337fe07ec4a90a21727a31
Johanna Wren <johanna.wren@noaa.gov>
2025-06-03 12:14:54 -1000

    Added code to read in CTD data files

--- CTD_exploration.R
+++ CTD_exploration.R
@@ -4,4 +4,29 @@
 # Description: Script to take an exploratory look at CTD data from SE-22-04 Hot Spot/Oxygen gradient cruise
 
 
-# load libraries
+# Load libraries
+library(dplyr)
+library(ggplot2)
+library(here)
+library(lubridate)
+
+# Set working directory
+myDir <- paste(here(), 'CTD', 'CTD_processed', sep='/')
+setwd(myDir)
+
+# Read is the data
+ctd <- read.table('dSE-22-04_02_01.asc', sep = ',')
+# check data
+head(ctd)
+str(ctd)
+# Move first row to column names
+names(ctd) <- ctd[1,]
+ctd <- ctd[-1,]
+# check data
+head(ctd)
+str(ctd)
+#ctd[,4:ncol(ctd)] <- as.numeric(ctd)
+
+# Convert date to date format and charaters to numeric
+ctd %>% mutate(Date=as.Date)
+  mutate_if(is.character, as.numeric)
