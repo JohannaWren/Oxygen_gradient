94c6ae517c391de81337a09740d5e6a6b8af029a
Johanna Wren <johanna.wren@noaa.gov>
2025-06-03 09:58:39 -1000

    adding basic code

---
+++ CTD_exploration.R
@@ -0,0 +1,7 @@
+# Date: June 3, 2025
+# Author: Johanna LK Wren & Emma Scott-Wellman
+# email: johanna.wren@noaa.gov emma.scott-wellman@noaa.gov
+# Description: Script to take an exploratory look at CTD data from SE-22-04 Hot Spot/Oxygen gradient cruise
+
+
+# load libraries
