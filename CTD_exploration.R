3fe1fc64885e39c9fe0d3c4e091f32c04690b93c
Johanna Wren <johanna.wren@noaa.gov>
2025-06-03 15:50:02 -1000

    simplified script for files without the special character in the column header

--- CTD_exploration.R
+++ CTD_exploration.R
@@ -15,30 +15,26 @@ myDir <- paste(here(), 'CTD', 'CTD_processed', sep='/')
 setwd(myDir)
 
 # Read is the data
-#ctd <- read.table('dSE-22-04_02_01.asc', sep = ',', header = F)
-ctd <- read.csv('dSE-22-04_02_01.asc', skip = 1, header = F)
+ctd <- read.csv('dSE-22-04_02_01.asc')
 # check data
 head(ctd)
 str(ctd)
 # Add header (hard coded in for now because special character is giving us issues)
-hdrstr <- c('Scan', 'Date', 'Time', 'Pressure', 'Conductivity', 'Oxygen_raw', 'Temperature', 'Flourescence', 'Lat', 'Lon', 'SigmaTheta', 'Depth', 'Oxygen_umKg', 'Salinity', 'Flag')
-names(ctd) <- hdrstr
-# check data
-head(ctd)
-str(ctd)
+#hdrstr <- c('Scan', 'Date', 'Time', 'Pressure', 'Conductivity', 'Oxygen_raw', 'Temperature', 'Flourescence', 'Lat', 'Lon', 'SigmaTheta', 'Depth', 'Oxygen_umKg', 'Salinity', 'Flag')
+#names(ctd) <- hdrstr
 
 # Convert date to date format and charaters to numeric
-ctd$DateTime <- as.POSIXct(paste(ctd$Date, ctd$Time), format='%m/%d/%Y %H:%M:%S') 
+ctd$DateTime <- as.POSIXct(paste(ctd$mm.dd.yyyy, ctd$hh.mm.ss), format='%m/%d/%Y %H:%M:%S') 
 # remove missing oxygen data 
-ctd$Oxygen_cleaned <- ctd$Oxygen_umKg
+ctd$Oxygen_cleaned <- ctd$Sbox0Mm.Kg
 ctd$Oxygen_cleaned[which(ctd$Oxygen_cleaned < 0)] <- NA
 
 # make a depth profile
-ggplot(ctd, aes(x=Oxygen_cleaned, y=Depth)) + 
+ggplot(ctd, aes(x=Oxygen_cleaned, y=DepSM)) + 
   geom_path() +
   scale_y_reverse()
 
 # Find the depth of the oxygen minimum
 idx <- which(ctd$Oxygen_cleaned == min(ctd$Oxygen_cleaned, na.rm = T))
-O2min <- ctd[idx,'Depth']
+O2min <- ctd[idx,'DepSM']
 
