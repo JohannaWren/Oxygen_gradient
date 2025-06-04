899c427588c9fb9eac0174fbcdca672696a15fdf
Johanna Wren <johanna.wren@noaa.gov>
2025-06-03 14:49:05 -1000

    added code to make a depth profile and find oxygen minimum depth

--- CTD_exploration.R
+++ CTD_exploration.R
@@ -15,18 +15,30 @@ myDir <- paste(here(), 'CTD', 'CTD_processed', sep='/')
 setwd(myDir)
 
 # Read is the data
-ctd <- read.table('dSE-22-04_02_01.asc', sep = ',')
+#ctd <- read.table('dSE-22-04_02_01.asc', sep = ',', header = F)
+ctd <- read.csv('dSE-22-04_02_01.asc', skip = 1, header = F)
 # check data
 head(ctd)
 str(ctd)
-# Move first row to column names
-names(ctd) <- ctd[1,]
-ctd <- ctd[-1,]
+# Add header (hard coded in for now because special character is giving us issues)
+hdrstr <- c('Scan', 'Date', 'Time', 'Pressure', 'Conductivity', 'Oxygen_raw', 'Temperature', 'Flourescence', 'Lat', 'Lon', 'SigmaTheta', 'Depth', 'Oxygen_umKg', 'Salinity', 'Flag')
+names(ctd) <- hdrstr
 # check data
 head(ctd)
 str(ctd)
-#ctd[,4:ncol(ctd)] <- as.numeric(ctd)
 
 # Convert date to date format and charaters to numeric
-ctd %>% mutate(Date=as.Date)
-  mutate_if(is.character, as.numeric)
+ctd$DateTime <- as.POSIXct(paste(ctd$Date, ctd$Time), format='%m/%d/%Y %H:%M:%S') 
+# remove missing oxygen data 
+ctd$Oxygen_cleaned <- ctd$Oxygen_umKg
+ctd$Oxygen_cleaned[which(ctd$Oxygen_cleaned < 0)] <- NA
+
+# make a depth profile
+ggplot(ctd, aes(x=Oxygen_cleaned, y=Depth)) + 
+  geom_path() +
+  scale_y_reverse()
+
+# Find the depth of the oxygen minimum
+idx <- which(ctd$Oxygen_cleaned == min(ctd$Oxygen_cleaned, na.rm = T))
+O2min <- ctd[idx,'Depth']
+
