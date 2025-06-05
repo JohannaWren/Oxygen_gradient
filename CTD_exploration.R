3819bee8721f22aef038e1e265121754a4978249
escott-wellman <emma.scott-wellman@noaa.gov>
2025-06-05 11:24:27 -1000

    Added 02 minimum

--- CTD_exploration.R
+++ CTD_exploration.R
@@ -13,9 +13,11 @@ library(tidyr)
 library(data.table)
 
 # Set working directory
-#myDir <- paste(here(), 'CTD', 'CTD_processed', sep='/') # Emma's file path
-myDir <- paste(here(), 'CTD', 'CTD_processed', sep='/')  # Johanna's File path
+myDir <- paste(here(),'CTD_processed_headers', sep='/') # Emma's file path
+#myDir <- paste(here(), 'CTD', 'CTD_processed', sep='/')  # Johanna's File path
 setwd(myDir)
+
+StnID <- c()
 
 # Read is the data
 # Single file
@@ -33,7 +35,7 @@ for (i in seq_along(files)) {
   # ctd_list[[i]]$Oxygen_cleaned[which(ctd_list[[i]]$Oxygen_cleaned < 0)] <- NA
 }
 # make index of eDNA casts to exclude
-idx <- c(14, 17)
+idx <- c(14, 16, 17, 20, 23, 26, 29, 32, 35, 38, 41, 42, 44, 46)
 
 # check data
 head(ctd_list[[10]])
@@ -47,6 +49,10 @@ ctdAll$DateTime <- as.POSIXct(paste(ctdAll$mm.dd.yyyy, ctdAll$hh.mm.ss), format=
 ctdAll$Oxygen_cleaned <- ctdAll$Sbox0Mm.Kg
 ctdAll$Oxygen_cleaned[which(ctdAll$Oxygen_cleaned < 0)] <- NA
 
+# make index of eDNA casts to exclude
+test <- ctdAll %>% group_by(.id) %>% slice(which.max(DepSM)) %>% select(.id, DepSM) %>% filter(DepSM<1000)
+idx <- test$.id
+
 # make a depth profile
 # this plots all of the profiles on one plot
 ggplot(ctdAll, aes(x=Oxygen_cleaned, y=DepSM)) + 
@@ -54,9 +60,9 @@ ggplot(ctdAll, aes(x=Oxygen_cleaned, y=DepSM)) +
   scale_y_reverse()
 
 # Find the depth of the oxygen minimum
-idx <- which(ctd$Oxygen_cleaned == min(ctd$Oxygen_cleaned, na.rm = T))
-O2min <- ctd[idx,'DepSM']
-O2min
+#idy <- which(ctdAll$Oxygen_cleaned == min(ctd$Oxygen_cleaned, na.rm = T))
+#O2min <- ctdAll[idy,'DepSM']
+#O2min
 
 # make a multipanel plot with multiple variables from one station
 # put data into "long" format
@@ -69,6 +75,12 @@ ggplot(ctd_long, aes(value, DepSM, color=varName)) +
   facet_wrap(.~varName, scales = 'free_x') + 
   scale_y_reverse()
 
+o2_min <- ctdAll %>% 
+  filter(! .id %in% idx) %>% 
+  group_by(.id) %>% 
+  slice(which.min(Oxygen_cleaned)) %>% select(.id, DepSM)   #summarize(min(Oxygen_cleaned, na.rm = T))
+
+#y_min <- ctdAll[which(ctdAll$Oxygen_cleaned == min(ctdAll$Oxygen_cleaned, na.rm=T)), 'DepSM']
 # make a multi-panel plot with one variable from all stations
 ctdAll %>% 
   filter(! .id %in% idx) %>% 
@@ -76,8 +88,24 @@ ctdAll %>%
     geom_path() +
     scale_y_reverse() + facet_wrap(.~.id) +
     theme_bw() +
-    theme(panel.grid.major = element_blank())
+    theme(panel.grid.major = element_blank()) +
+    geom_hline(data = o2_min, aes(yintercept = DepSM), color='black')
 
-ggsave('SingleVarAllStn.png', width=11, height = 8, dpi = 300, units = 'in')
+ggsave('O2DepthProfiles_AllStns.png', width=11, height = 8, dpi = 300, units = 'in')
 
+# Pull Id's for station matching
+> test2 <- ctdAll %>% group_by(.id) %>% slice(which.min(DateTime)) %>% select(DateTime) %>% data.frame()
+> test2$DateTime <- test2$DateTime - (60*60*10)
+> test2
 
+#
+ctdAll %>% 
+  filter(! .id %in% idx) %>% 
+  ggplot(aes(x=, y=DepSM, color=as.factor(.id))) + 
+  geom_path() +
+  scale_y_reverse() + facet_wrap(.~.id) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank()) +
+  geom_hline(data = o2_min, aes(yintercept = DepSM), color='black')
+
+ggsave('O2DepthProfiles_AllStns.png', width=11, height = 8, dpi = 300, units = 'in')
