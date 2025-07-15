e42e760ece9e130bed792f489688f63070a3f3e9
escott-wellman <emma.scott-wellman@noaa.gov>
2025-07-14 15:09:43 -1000

    Starting an eDNA script

---
+++ eDNA.R
@@ -0,0 +1,7 @@
+# Date: July 14, 2025
+# Author: Johanna LK Wren & Emma Scott-Wellman
+# email: johanna.wren@noaa.gov emma.scott-wellman@noaa.gov
+# Description: RScript for eDNA Data 
+
+readRDS('Reads_PhyloseqObject.RDS')
+readRDS('ZOTU_PhyloseqObject.RDS')
